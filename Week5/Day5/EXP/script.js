00ccb4d6a0650b73f0644cada1f82e0ac03d1968
Alex Kuzmin <alexkuzmin@Alexs-MacBook-Air.local>
2022-01-06 21:40:34 +0200

    w5 d5 mini proj starwars

--- Week5/Day5/EXP/script.js
+++ Week5/Day5/EXP/script.js
@@ -8,60 +8,37 @@
 // to display the info on the DOM: the name, height, gender, birth year, and home world of the character.
 
 // Display the data using AJAX. Make sure to display a loading message as follows:
-// You can use any of these animation icons for the loading message.
 
-function retrieveElements() {
+const retrieveElements = (() => {
   const findBtn = document.querySelector("button");
   const heroCard = document.querySelector(".heroCard");
-  findBtn.addEventListener("click", displayData);
-  return [findBtn, heroCard];
-}
+  findBtn.addEventListener("click", function () {
+    displayData(heroCard);
+  });
+})();
 
 async function getData() {
-  const ranNum = Math.round(Math.random() * 9);
-  //   console.log(ranNum);
+  const ranNum = Math.round(Math.random() * 9 + 1);
+  console.log(ranNum);
   const dataPeopleFetched = await fetch(
-    `https://swapi.dev/api/people/${ranNum}
+    `https://www.swapi.tech/api/people/${ranNum}
   `
   );
   const charObj = await dataPeopleFetched.json();
-
-  //   console.log(peopleObj);
   return charObj;
 }
 
-async function displayData() {
-  //   const findBtn = retrieveElements()[0];
-  const heroCard = retrieveElements()[1]; //I can simply append it on the body. no need for this const. without it the first function can be self invoked
+async function displayData(heroCard) {
   const charObj = await getData();
   console.log(charObj);
-  const heroDiv = document.createElement("div");
-  const heroPara = document.createElement("p");
-  const h1Name = document.createElement("h1");
-  h1Name.textContent = charObj.name;
-  const h6Height = document.createElement("h6");
-  h6Height.textContent = `Height: ${charObj.height}`; //do I need to get the key name also in a comp way?
-  const h5Gender = document.createElement("h5");
-  h5Gender.textContent = `Gender: ${charObj.gender}`;
-  const h4birthYear = document.createElement("h4");
-  h4birthYear.textContent = `Birth Year:${charObj.birth_year}`;
-  const h3HomeWorld = document.createElement("h3");
-  h3HomeWorld.textContent = `Home World:${charObj.homeworld}`; //do I need to get them dinamically?
-  heroDiv.append(h1Name, h6Height, h5Gender, h4birthYear, h3HomeWorld);
-  heroCard.append(heroDiv);
+  let spinner = `<i class="fas fa-spinner fa-spin fa-8x" id="spinner"></i>`;
+  heroCard.innerHTML = spinner;
+  const homeWorld = await (
+    await fetch(`${charObj.result.properties.homeworld}`)
+  ).json();
+  const para = document.createElement("p");
+  heroCard.innerHTML = `<h1>${charObj.result.properties.name}</h1>Height: &nbsp${charObj.result.properties.height}<br>
+  Gender: &nbsp ${charObj.result.properties.gender}<br>
+  Birth Year: &nbsp${charObj.result.properties.birth_year}<br>
+  Home World: &nbsp${homeWorld.result.properties.name}`;
 }
-
-// async function fetchPlanets() {
-//   const fetched = await (await fetch("https://swapi.dev/api/planets")).json();
-//   console.log(fetched);
-// }
-retrieveElements();
-// getData();
-// displayData();
-// fetchPlanets();
-
-//1. self invoke the first fun
-//2. when pressed on the btn again, the first card content is replaced by another
-//put the card at the center
-//background
-//display planet name at the display fun (probably hard coded)
