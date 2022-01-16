6b668b5811d6f02bca26460de0fc176fdff6bcb7
Alex Kuzmin <alexkuzmin@Alexs-MacBook-Air.local>
2022-01-16 13:01:06 +0200

    node.j beg EXP

---
+++ Week7/Day1/ExerciseXP/main.js
@@ -0,0 +1,21 @@
+///exe 1
+// let a = 5;
+// let b = 10;
+// console.log(`hello you are ${a + b}`);
+
+///exe 2
+console.log("Hi, thanks for running me with npm start!");
+
+///exe 3
+let a = 5;
+let b = 10;
+let x;
+let c = 6;
+x = a + b;
+console.log("first run:", x);
+console.log("second run:", x + c);
+
+// Add some more code in the main.js so when you run it using nodemon, it logs the sum of a + b as a first run.
+// Tip: you need to change the package.json file in order to achieve this.
+
+// Update the main.js file to console.log the sum of x + c
