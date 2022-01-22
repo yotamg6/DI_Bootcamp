d89dd130bab78e35a991ad66fc4d141ccc4c36ef
Alex Kuzmin <alexkuzmin@Alexs-MacBook-Air.local>
2022-01-22 19:09:42 +0200

    RSS ejs minipro

---
+++ Week7/Day5/ExerciseXP/xp2/server.js
@@ -0,0 +1,52 @@
+const exp = require("express");
+const axios = require("axios");
+const env = require("dotenv");
+// const cors = require("cors");//doesn't let me render anything
+// app.use(cors);//moved it here after commenting-out
+let Parser = require("rss-parser");
+let parser = new Parser();
+
+const app = exp();
+
+app.use(exp.urlencoded());
+app.use(exp.json());
+
+env.config();
+
+app.set("view engine", "ejs");
+
+(async () => {
+  let feed = await parser.parseURL("https://www.thefactsite.com/feed/");
+  //   console.log("website title:", feed.title);
+  //   console.log("items:", feed.items);
+  feed.items.forEach((item) => {
+    console.log(item.title + ":" + item.link);
+  });
+  const items = feed.items;
+  app.get("/posts", (req, res) => {
+    res.render("partials/posts", { posts: items });
+  });
+  app.get("/search", (req, res) => {
+    res.render("pages/search", { posts: items });
+  });
+})();
+
+app.get("/home", (req, res) => {
+  res.render("pages/index");
+});
+
+app.post("search/title", (req, res) => {
+  console.log(req.body);
+  const chosTitle = req.body;
+  res.render("pages/search", { title: chosTitle });
+});
+
+app.post("search/category", (req, res) => {
+  console.log(req.body);
+  const chosCategory = req.body;
+  res.render("pages/search", { category: chosCategory });
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`listening to ${process.env.PORT}`)
+);
