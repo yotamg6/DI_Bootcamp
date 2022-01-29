183fab9425b3b7c811f73a5f551894fdb305417e
Alex Kuzmin <alexkuzmin@Alexs-MacBook-Air.local>
2022-01-29 15:08:04 +0200

    memory-app DC w8 d5

---
+++ Week8/Day5/DailyChallenge/memory/src/App.js
@@ -0,0 +1,86 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import supers from "./superheroes";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      score: 0,
+      highestScores: new Set(),
+    };
+  }
+
+  shuffle = () => {
+    const superheroes = supers.superheroes;
+    let currentIndex = superheroes.length,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [superheroes[currentIndex], superheroes[randomIndex]] = [
+        superheroes[randomIndex],
+        superheroes[currentIndex],
+      ];
+    }
+    return superheroes;
+  };
+
+  handleClick = (heroObj) => {
+    if (heroObj.clicked) {
+      supers.superheroes.map((hero) => {
+        hero.clicked = false;
+      });
+      this.setState({ score: 0 });
+    } else {
+      heroObj.clicked = true;
+      const score = this.state.score + 1;
+      const highestScores = this.state.highestScores;
+      highestScores.add(score);
+      this.setState({ score: score, highestScores: highestScores });
+    }
+  };
+
+  render() {
+    console.log("length:", this.state.highestScores.size);
+    const shuffled = this.shuffle();
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Top score: {this.state.highestScores.size} </p>
+          <p>Score: {this.state.score} </p>
+          {shuffled.map((hero, i) => {
+            return (
+              <div
+                key={i}
+                className="card"
+                onClick={() => this.handleClick(hero)}
+              >
+                <div className="imageContainer">
+                  <img src={hero.image}></img>
+                  <ul>
+                    <li>
+                      <strong>{hero.name}</strong>
+                    </li>
+                    <li>
+                      <strong>Occupation:</strong>
+                      {hero.occupation}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            );
+          })}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
