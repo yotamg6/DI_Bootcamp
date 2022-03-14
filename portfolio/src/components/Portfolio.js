be558f7e9c8c8d0fbb57368967e95366f57a9ee9
yotamg6 <93833968+yotamg6@users.noreply.github.com>
2022-03-14 15:01:17 +0200

    links updated

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { Link } from "react-router-dom";
 
 import {
   Box,
@@ -18,8 +17,8 @@ import {
 const projects = [
   {
     name: "Woof Cupid",
-    description: `Dating website for dogs. Find the right breed for you and your dog, and match with other dogs accordingly. Finding true love has never been so precise! React, JWT, PostgreSQL, Sequelize, MUI`,
-    url: "http://localhost:3000/project_screenshots/woof-cupid.png",
+    description: `Dating website for dogs. Find the right breed for you and your dog, and match with other dogs accordingly. Finding true love has never been so precise! React, PostgreSQL, JWT, AWS S3, Sequelize, MUI`,
+    img: "/project_screenshots/woof-cupid.png",
     gitHub:
       "https://github.com/yotamg6/DI_Bootcamp/tree/master/Final_Project/final-project",
     deployed: "https://woof-cupid.herokuapp.com/",
@@ -27,25 +26,25 @@ const projects = [
   {
     name: "Movie-App",
     description: `Search any movie and get full details, including posters and links to IMDB. React, Redux`,
-    url: "http://localhost:3000/project_screenshots/movie-app.png",
+    img: "/project_screenshots/movie-app.png",
     gitHub:
       "https://github.com/yotamg6/DI_Bootcamp/tree/master/Week9/Day5/DailyChallenge/movie-app",
-    deployed: "",
+    video: "https://www.loom.com/share/9a286ebe4eed432ab9e193313de00221",
   },
   {
     name: "Tic-Tac-Toe",
     description: `The classic game with two play modes, including one against AI. can you beat the machine? React.js`,
-    url: "http://localhost:3000/project_screenshots/tic-tac-toe.png",
+    img: "/project_screenshots/tic-tac-toe.png",
     gitHub:
       "https://github.com/yotamg6/DI_Bootcamp/tree/master/Week10/Day1/exp2/tic-tac-toe",
-    deployed: "",
+    video: "",
   },
   {
     name: "Guess Who I Am",
     description: `Try and guess who is the mysterious celebrity. Ask the program questions and you will be answered! Pure HTML, CSS, Javascript`,
-    url: "http://localhost:3000/project_screenshots/guess-who.png",
+    img: "/project_screenshots/guess-who.png",
     gitHub: "https://github.com/yotamg6/guess-who-I-am",
-    deployed: "",
+    video: "",
   },
 ];
 
@@ -64,25 +63,46 @@ const Portfolio = () => {
       <ImageList sx={{ m: 2 }}>
         {projects.map((project, i) => (
           <Box className="cardContainerFolio" key={i}>
-            <CardMedia component="img" alt="Project img" image={project.url} />
+            <CardMedia component="img" alt="Project img" src={project.img} />
             <CardContent>
-              <Link underline="none" href={`${project.deployed}`}>
-                <Typography variant="h5" style={{ color: "tomato" }}>
-                  {project.name}
-                </Typography>
-              </Link>
+              <Typography variant="h5" style={{ color: "tomato" }}>
+                {project.name}
+              </Typography>
               <Typography variant="body2" fontSize="17px">
                 {project.description}
               </Typography>
             </CardContent>
 
+            {project.deployed ? (
+              <CardActions>
+                <Link
+                  href={`${project.deployed}`}
+                  style={{ color: "#8D7B9C", fontWeight: "bold" }}
+                  underline="none"
+                >
+                  DEMO
+                </Link>
+              </CardActions>
+            ) : (
+              <CardActions>
+                <Link
+                  href={`${project.video}`}
+                  style={{ color: "#8D7B9C", fontWeight: "bold" }}
+                  underline="none"
+                >
+                  DEMO
+                </Link>
+              </CardActions>
+            )}
             <CardActions>
-              <Link href={`${project.gitHub}`}>Source code</Link>
+              <Link
+                href={`${project.gitHub}`}
+                style={{ color: "#8D7B9C", fontWeight: "bold" }}
+                underline="none"
+              >
+                SOURCE CODE
+              </Link>
             </CardActions>
-            {/* <Button size="small" color="primary">
-                  Live Demo
-                </Button>
-            </CardActions>  */}
           </Box>
         ))}
       </ImageList>
